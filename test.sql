37608b8321db8dbe873aadf22c99887fae9aa911
lvrst <leo.verset@gmail.com>
2021-06-08 15:33:32 +0200

    Ajouté table category et et quiz

--- test.sql
+++ test.sql
@@ -1,5 +1,24 @@
 CREATE TABLE question
 (
-    id INT PRIMARY KEY NOT NULL
+    id INT PRIMARY KEY NOT NULL,
+    `image` VARCHAR (190),
+    `question` VARCHAR (190),
+    `answer` BOOLEAN, 
+    `explanation` TEXT
+    /** Quiz_id ???*/
+);
 
-)
+CREATE TABLE category
+(
+    id INT PRIMARY KEY NOT NULL,
+    `name` VARCHAR (100),
+    `color` VARCHAR (100),
+    `icon` VARCHAR (190), 
+    `image` VARCHAR (190)
+);
+
+CREATE TABLE Quiz_id
+(
+
+    /** Quiz_id ???*/
+);
