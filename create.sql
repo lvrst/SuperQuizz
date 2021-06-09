5cbe4541d83f027a453372f3a09e186024c507f6
lvrst <leo.verset@gmail.com>
2021-06-09 09:46:00 +0200

    J'ai changé la syntaxe de commentaires     Afin qu'elle passe en langage SQL

--- create.sql
+++ create.sql
@@ -1,23 +1,21 @@
-/** Je crée la Base De Données qui contiendra les tables 
-    Si elle n'existe pas déjà
-    La nommer 'quiz_bdd'
-*/ 
+-- Je crée la Base De Données qui contiendra les tables 
+-- Si elle n'existe pas déjà
+-- La nommer 'quiz_bdd'
 CREATE DATABASE IF NOT EXISTS quiz_bdd;
 
-/** Je crée la Table 'Categories' en premier lieu
-    Ainsi, elle pourra être référencée pour les dépendances
- */
+--Je crée la Table 'Categories' en premier lieu
+--Ainsi, elle pourra être référencée pour les dépendances
+
 CREATE TABLE IF NOT EXISTS Categories
 (
-    /** Je crée l'ID
-    C'est la Clé Primaire de l'entité 'Category'
-    */
+    -- Je crée l'ID
+    -- C'est la Clé Primaire de l'entité 'Category'
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    /** Chaque entité 'Category' possède :
-    - un nom
-    - une couleur utilisée en background d'entête et pour l'icône
-    - une icone
-    - une image présentée en vignette de la catégorie */
+    -- Chaque entité 'Category' possède :
+    -- un nom
+    -- une couleur utilisée en background d'entête et pour l'icône
+    -- une icone
+    -- une image présentée en vignette de la catégorie
     `name` VARCHAR (100),
     `color` VARCHAR (100),
     `icon` VARCHAR (190), 
@@ -30,11 +28,11 @@ CREATE TABLE IF NOT EXISTS Quizzes
     id INT PRIMARY KEY NOT NULL,
     `name` VARCHAR (100),
     `image` VARCHAR (190),
-    /**Je crée l'emplacement de ma clé étrangère 
-    En précisant son nom et son type de données */
+    -- Je crée l'emplacement de ma clé étrangère 
+    -- En précisant son nom et son type de données */
     `category_id` INT,
-    /**Je génère la clé étrangère
-    Je précise le moteur InnoDB */
+    -- Je génère la clé étrangère
+    -- Je précise le moteur InnoDB
     FOREIGN KEY (category_id) REFERENCES Categories(id) 
 ) ENGINE=InnoDb;
 
